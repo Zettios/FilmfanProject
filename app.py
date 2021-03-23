cee3cde97d283d0e0250840cae5d8f200b584a75
Zettios <enricohuig2@gmail.com>
2021-03-23 19:09:33 +0100

    Implemented page linking & basic info for screens

--- app.py
+++ app.py
@@ -4,10 +4,35 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'mijngeheimesleutel'
 
+class Film:
+    def __init__(self, film_id, titel, regisseur, jaar):
+        self.film_id = film_id
+        self.titel = titel
+        self.regisseur = regisseur
+        self.jaar = jaar
+
+film1 = Film(1, "foo1", "bar1", 1991)
+film2 = Film(2, "foo2", "bar2", 2055)   
+film3 = Film(3, "foo3", "bar3", 2001)   
+film4 = Film(4, "foo4", "bar4", 1890)
+
+films = [film1, film2, film3, film4]
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', len = len(films), Films = films)
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/film')
+def film():
+    return render_template('film.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
